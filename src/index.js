f07b8dc85dabab9ae17cdbdfbb33a72abfe0b53d
respektive <liambzw@gmail.com>
2021-08-05 21:15:31 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -101,10 +101,9 @@ app.on('ready', function () {
 
  
 
-  // and load the index.html of the app.
+
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
-  // Open the DevTools.
 /*   mainWindow.webContents.openDevTools(); */
 
 
@@ -387,9 +386,7 @@ getUserBest().then(userBest => {
 
 	});
 }
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
@@ -397,14 +394,9 @@ app.on('window-all-closed', () => {
 });
 
 app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
 
 
